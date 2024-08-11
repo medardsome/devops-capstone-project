0abef8bf092848b22f27715340f9d2130dee17ef
medardsome <medard.some@gmail.com>
2024-08-11 02:54:36 -0400

    completed ci build3
    completed ci build3

--- service/routes.py
+++ service/routes.py
@@ -63,7 +63,7 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 
-@app.route("/accounts", methods=["GET"])
+  @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
     List all Accounts
