de6a3a2c121205f7ab839765abb1875764c48e45
medardsome <medard.some@gmail.com>
2024-08-11 03:10:36 -0400

    completed ci build5
    completed ci build5

--- service/routes.py
+++ service/routes.py
@@ -101,7 +101,7 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# place you code here to UPDATE an account
+# place you code here to UPDATE an account h
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -124,7 +124,7 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-# place you code here to DELETE an account
+# place you code here to DELETE an account g
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
